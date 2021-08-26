d5d4e52b851bea43ec2640f66c8b87d0597e18fa
hackerone <hackerone@hackerone.hackerone>
2021-08-26 13:31:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SnS_Assign1
---
+++ prg1.py
@@ -0,0 +1,43 @@
+import sys
+
+def common_divisors(args, n):
+	
+	ret_val = []
+	
+	minimum = args[0]
+	
+	for i in range(0, n):
+		minimum = min(args[i], minimum)
+	
+	for i in range(2, minimum+1):
+		
+		flag = 1
+
+		for j in range(0, n):
+			
+			if(args[j]%i != 0):
+				flag = 0
+
+		if(flag):
+			ret_val.append(i)
+	
+	return ret_val		
+
+def __main__():
+	
+	args = sys.argv
+	
+	n = int(args[1])
+	
+	l = [int(args[i]) for i in range(2, n+2)]
+	
+	#print(l)
+
+	l = common_divisors(l, n)
+	
+	for i in l:
+		print(i, end=" ")
+
+
+__main__()
+
